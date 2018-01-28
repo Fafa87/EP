2a703f18948b00d6d816af218bd6d2242b23540c
qwerty <fafafft@gmail.com>
2018-01-29 00:41:06 +0100

    adding dummy unit test

---
+++ tests/test_single_evaluation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import math
+import unittest
+
+import ep.compare
+import ep.evaluate
+
+class TestSingleEvaluation(unittest.TestCase):
+    def test_single_evaluation(self):
+        pass
