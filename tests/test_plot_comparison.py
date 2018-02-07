a2d3eab95d5096e35262f9cf8e601664e9cecb1d
Fafa <fafafft@gmail.com>
2018-02-07 10:10:51 +0100

    unit test fixing and reformating

--- tests/test_plot_comparison.py
+++ tests/test_plot_comparison.py
@@ -1,8 +1,9 @@
-import unittest
-import sys
 import os
+import sys
+import unittest
 
 from ep.evalplatform import plot_comparison
+from ep.evalplatform.yeast_datatypes import *
 
 
 class TestColours(unittest.TestCase):
@@ -10,31 +11,51 @@ class TestColours(unittest.TestCase):
     # GT = 3 | 2 -> 4 | 3
     # Algo = 2 | 2 -> 3 | 2
     dictionary_colour = {}
+
     def setUp(self):
-        self.dictionary_colour = dict([((1,1),0),((1,2),0),((1,3),3),((1,4),3),((1,5),0),((2,1),0),((2,2),0),((2,3),3),((2,4),3),((2,5),0),((2,6),0),((2,7),3)])
-        self.frame0GT = [(1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5)]
-        self.frame1GT = [(1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5),(6,6,6),(7,7,7)]
-        self.frame0Res = [(1,1,1),(2,2,2),(3,3,3),(4,4,4)]
-        self.frame1Res = [(1,1,1),(2,2,2),(5,5,5),(3,3,3),(7,7,7)]
+        def map_to_cells(frame, old_cell_data):
+            return [CellOccurence(frame, old_data[0], old_data[0], (old_data[1], old_data[2])) for old_data in
+                    old_cell_data]
 
-        self.frame0Mapping = [((1,1,1),(1,1,1)),((2,2,2),(2,2,2)),((3,3,3),(3,3,3)),((4,4,4),(4,4,4))]
-        self.frame1Mapping = [((1,1,1),(1,1,1)),((2,2,2),(2,2,2)),((3,3,3),(3,3,3)),((5,5,5),(5,5,5)),((7,7,7),(7,7,7))]
+        self.dictionary_colour = dict(
+            [((1, 1), 0), ((1, 2), 0), ((1, 3), 3), ((1, 4), 3), ((1, 5), 0), ((2, 1), 0), ((2, 2), 0), ((2, 3), 3),
+             ((2, 4), 3), ((2, 5), 0), ((2, 6), 0), ((2, 7), 3)])
+        self.frame0GT = map_to_cells(1, [(1, 1, 1), (2, 2, 2), (3, 3, 3), (4, 4, 4), (5, 5, 5)])
+        self.frame1GT = map_to_cells(2, [(1, 1, 1), (2, 2, 2), (3, 3, 3), (4, 4, 4), (5, 5, 5), (6, 6, 6), (7, 7, 7)])
+        self.frame0Res = map_to_cells(1, [(1, 1, 1), (2, 2, 2), (3, 3, 3), (4, 4, 4)])
+        self.frame1Res = map_to_cells(2, [(1, 1, 1), (2, 2, 2), (5, 5, 5), (3, 3, 3), (7, 7, 7)])
 
+        self.frame0Mapping = [(self.frame0GT[0], self.frame0Res[0]), (self.frame0GT[1], self.frame0Res[1]),
+                              (self.frame0GT[2], self.frame0Res[2]), (self.frame0GT[3], self.frame0Res[3])]
+        self.frame1Mapping = [(self.frame1GT[0], self.frame1Res[0]), (self.frame1GT[1], self.frame1Res[1]),
+                              (self.frame1GT[2], self.frame1Res[3]),
+                              (self.frame1GT[4], self.frame1Res[2]), (self.frame1GT[6], self.frame1Res[4])]
+
+    def apply_colours(self, data, colours):
+        for cell in data:
+            key = (cell.frame_number, cell.cell_id)
+            if key in colours:
+                cell.colour = colours[key]
+            else:
+                cell.colour = 0
 
     def test_calculate_stats_tracking(self):
         # (last_gt,last_res),last_mapping,(new_gt,new_res),new_mapping
         # len(found_links), len(real_links), len(correct_links))
-        plot_comparison.frame_id = 2
-        plot_comparison.dictionary_colour = self.dictionary_colour
-        wyniki = plot_comparison.calculate_stats_tracking((self.frame0GT,self.frame0Res),self.frame0Mapping,(self.frame1GT,self.frame1Res),self.frame1Mapping)
-        self.assertEqual((2,3,2),wyniki)
+        self.apply_colours(self.frame1Res, self.dictionary_colour)
+        self.apply_colours(self.frame1GT, self.dictionary_colour)
+        wyniki = plot_comparison.calculate_stats_tracking((self.frame0GT, self.frame0Res), self.frame0Mapping,
+                                                          (self.frame1GT, self.frame1Res), self.frame1Mapping)
+        self.assertEqual([2, 3, 2], map(len, wyniki[:3]))
         # precision = 100%
         # recall = 66%
 
         # no colours
-        plot_comparison.dictionary_colour = dict([(a,0) for (a,x) in self.dictionary_colour.items()])
-        wyniki = plot_comparison.calculate_stats_tracking((self.frame0GT,self.frame0Res),self.frame0Mapping,(self.frame1GT,self.frame1Res),self.frame1Mapping)
-        self.assertEqual((3,5,3),wyniki)
+        self.apply_colours(self.frame1Res, {})
+        self.apply_colours(self.frame1GT, {})
+        wyniki = plot_comparison.calculate_stats_tracking((self.frame0GT, self.frame0Res), self.frame0Mapping,
+                                                          (self.frame1GT, self.frame1Res), self.frame1Mapping)
+        self.assertEqual([3, 5, 3], map(len, wyniki[:3]))
         # precision = 100%
         # recall = 60%
 
@@ -44,31 +65,38 @@ class TestColours(unittest.TestCase):
         Input: [(cell_id, position_x, position_y)] x2
         Result: (cell_count_results, cell_count_ground_truth, correspondences, false_positives, false_negatives)
         """
-        plot_comparison.frame_id = 1
-        plot_comparison.dictionary_colour = self.dictionary_colour
-        wyniki = plot_comparison.calculate_stats_segmentation(self.frame0GT,self.frame0Res)
-        self.assertEqual((2,3,2),wyniki[:3])
+        self.apply_colours(self.frame0Res, self.dictionary_colour)
+        self.apply_colours(self.frame0GT, self.dictionary_colour)
+        wyniki = plot_comparison.calculate_stats_segmentation(self.frame0GT, self.frame0Res)
+        self.assertEqual((2, 3), wyniki[:2])
+        self.assertEqual(2, len(wyniki[2]))
         # precision = 100%
         # recall = 66%
 
-        plot_comparison.dictionary_colour = dict([(a,0) for (a,x) in self.dictionary_colour.items()])
-        wyniki = plot_comparison.calculate_stats_segmentation(self.frame0GT,self.frame0Res)
-        self.assertEqual((4,5,4),wyniki[:3])
+        self.apply_colours(self.frame0Res, {})
+        self.apply_colours(self.frame0GT, {})
+        wyniki = plot_comparison.calculate_stats_segmentation(self.frame0GT, self.frame0Res)
+        self.assertEqual((4, 5), wyniki[:2])
+        self.assertEqual(4, len(wyniki[2]))
         # precision = 100%
         # recall = 80%!
 
-        plot_comparison.frame_id = 2
-        plot_comparison.dictionary_colour = self.dictionary_colour
-        wyniki = plot_comparison.calculate_stats_segmentation(self.frame1GT,self.frame1Res)
-        self.assertEqual((3,4,3),wyniki[:3])
+        self.apply_colours(self.frame1Res, self.dictionary_colour)
+        self.apply_colours(self.frame1GT, self.dictionary_colour)
+        wyniki = plot_comparison.calculate_stats_segmentation(self.frame1GT, self.frame1Res)
+        self.assertEqual((3, 4), wyniki[:2])
+        self.assertEqual(3, len(wyniki[2]))
         # precision = 100%
         # recall = 75%
 
-        plot_comparison.dictionary_colour = dict([(a,0) for (a,x) in self.dictionary_colour.items()])
-        wyniki = plot_comparison.calculate_stats_segmentation(self.frame1GT,self.frame1Res)
-        self.assertEqual((5,7,5),wyniki[:3])
+        self.apply_colours(self.frame1Res, {})
+        self.apply_colours(self.frame1GT, {})
+        wyniki = plot_comparison.calculate_stats_segmentation(self.frame1GT, self.frame1Res)
+        self.assertEqual((5, 7), wyniki[:2])
+        self.assertEqual(5, len(wyniki[2]))
         # precision = 100%
         # recall = 71%
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromModule(sys.modules["__main__"])
