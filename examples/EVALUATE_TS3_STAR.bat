0ed66205327a92a5777312f6ebe7a87ea47590e3
qwerty <fafafft@gmail.com>
2019-04-23 15:29:03 +0200

    fix usage

--- examples/EVALUATE_TS3_STAR.bat
+++ examples/EVALUATE_TS3_STAR.bat
@@ -1,3 +1,1 @@
-python evaluate.py TestSet3 "GroundTruth" "gt_seg" "gt_track" CellStar "CellStar" "cs_seg" "cs_track" 
-
-python evaluate.py TestSet3 GroundTruth all_segmentation.csv all_tracking.csv CellStar /S CellStar_all all_tracking.csv
+python evaluate.py TestSet3 "GroundTruth" "gt_seg" "gt_track" CellStar "CellStar" "cs_seg" "cs_track"
