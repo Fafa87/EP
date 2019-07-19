bc1dc615c3e6bf13daef37a71bbbd8d3f67a6878
qwerty <fafafft@gmail.com>
2019-07-20 01:01:43 +0200

    draw_details go fire

--- ep/evalplatform/draw_details.py
+++ ep/evalplatform/draw_details.py
@@ -3,6 +3,7 @@ import os
 import re
 
 import imageio
+import fire
 import scipy.misc as misc
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -38,7 +39,7 @@ class PaintRequestABC(object):
         pass      
 
 class DrawingOverlordABC(object):
-    def __init__(self, params):
+    def __init__(self, **kwargs):
         pass
     
     def help_params(self):
@@ -100,25 +101,19 @@ class TrackingDetail(PaintRequestABC):
 
 EvaluationType = Enum("SEGMENTATION","TRACKING","MISC")
 class EvaluationDetails(DrawingOverlordABC):
-    def __init__(self, params, fill_markers=False, markersize=7):
-        DrawingOverlordABC.__init__(self, params)
-        self.details_file = params[0]
-        self.required_substring = None
-        self.fill_markers=fill_markers
+    def __init__(self, details_file,
+                 required_substring=None, details_type=None, draw_correct=True,
+                 fill_markers=False, markersize=7, **kwargs):
+        DrawingOverlordABC.__init__(self, **kwargs)
+        self.details_file = details_file
+        self.required_substring = required_substring
+        self.fill_markers = fill_markers
         self.markersize = markersize
-        if(len(params)>1):
-            self.required_substring = params[1]
-        if(len(params)>2):
-            self.details_type = params[2]
-        else:
-            self.details_type = self.determine_type(self.details_file)
-            
-        if(len(params)>3):
-            self.draw_correct = params[3]
-        else:
-            self.draw_correct = True
-            
-    def determine_type(self, filepath):
+        self.details_type = details_type or EvaluationDetails.determine_type(self.details_type)
+        self.draw_correct = draw_correct
+
+    @staticmethod
+    def determine_type(filepath):
         if SEGDETAILS_SUFFIX in filepath:
             return EvaluationType.SEGMENTATION
         elif TRACKDETAILS_SUFFIX in filepath or LONGTRACKDETAILS_SUFFIX in filepath:
@@ -209,6 +204,7 @@ get_path_new_file = lambda directory,filename: os.path.join(directory,"".join([o
 
 # =============== SCRIPT USAGE PARAMETERS ================= #
 
+
 def get_trailing_number(filepath):
     return parse_file_order(get_trailing_order(filepath, is_path=True))
 
@@ -270,14 +266,12 @@ def run(overlord, directory_images, directory_output, desired_output_file_prefix
     
     debug_center.show_in_console(None, "Progress", "Done applying requests on input images...")
 
-if __name__== "__main__": 
-    if len(sys.argv) == 1:
-        print ("Parameters: input_images_directory, output_images_directory, output_file_prefix" + EvaluationDetails.help_params())
-        exit(0)
-    
-    directory_images = sys.argv[1]
-    directory_output = sys.argv[2]
-    output_file_prefix = sys.argv[3]
-    overlord = EvaluationDetails(sys.argv[4:], fill_markers=True, markersize=5)
-    run(overlord,directory_images,directory_output,output_file_prefix)
-    
+
+def main(input_images_directory, output_images_directory, output_file_prefix,
+         details_file, **kwargs):
+    overlord = EvaluationDetails(details_file=details_file, **kwargs)
+    run(overlord, input_images_directory, output_images_directory, output_file_prefix)
+
+
+if __name__== "__main__":
+    fire.Fire(main)
