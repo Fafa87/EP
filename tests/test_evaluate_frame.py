0985c7a77d2a3e85a18bdb3fe0e961834710de61
qwerty <fafafft@gmail.com>
2019-07-22 23:48:28 +0200

    test and travis fix

--- tests/test_evaluate_frame.py
+++ tests/test_evaluate_frame.py
@@ -2,8 +2,6 @@
 from copy import deepcopy
 from unittest import skip
 
-import numpy as np
-
 from ep import evaluate_frame
 from ep.evalplatform.yeast_datatypes import CellOccurence
 from tests.testbase import TestBase
@@ -25,20 +23,6 @@ class TestEvaluateFrame(TestBase):
                           7: CellOccurence(3, 6, 6, (300, 300)),
                           8: CellOccurence(3, 7, 7, (10, 350)),
                           10: CellOccurence(3, 5, 5, (250, 500))}
-
-    def save_in_platform_format(self, filename, cells):
-        with self.create_temp(filename) as file:
-            file.write("Frame_nr, Cell_nr, Cell_colour, Position_X, Position_Y\n")
-            for cell in cells:
-                file.write("{frame}, {cellid}, {color}, {posx}, {posy}\n".format(
-                    frame=cell.frame_number, cellid=cell.cell_id, posx=cell.position[0], posy=cell.position[1],
-                    color=cell.colour))
-
-    def save_in_mask_format(self, filename, cells, radius):
-        image = np.zeros((510, 510), dtype=np.uint8)
-        for cell in cells:
-            self.draw_cell(image, cell.position, radius, cell.colour + 1)
-        self.save_temp(filename, image)
 
     def test_single_evaluation_all(self):
         self.save_in_platform_format("gt.csv", self.gt_data.values())
@@ -102,16 +86,8 @@ class TestEvaluateFrame(TestBase):
         pass
 
     def test_single_evaluation_with_mask_format(self):
-        gt_data_1_frame = deepcopy(self.gt_data)
-        for gt in gt_data_1_frame.values():
-            gt.frame_number = 1
-
-        algo_data_1_frame = deepcopy(self.algo_data)
-        for algo in algo_data_1_frame.values():
-            algo.frame_number = 1
-
-        self.save_in_mask_format("gt.tif", gt_data_1_frame.values(), 4)
-        self.save_in_mask_format("algo.tif", algo_data_1_frame.values(), 4)
+        self.save_in_mask_format("gt.tif", self.gt_data.values(), 4)
+        self.save_in_mask_format("algo.tif", self.algo_data.values(), 4)
 
         metrics, details = evaluate_frame.evaluate_one_frame("gt.tif", "algo.tif", "Kartek", parser_symbol="MASK")
         self.assertEqual(2.0 / 7, metrics["Precision"])
@@ -120,21 +96,21 @@ class TestEvaluateFrame(TestBase):
 
         correct_pairs = [(c.cell_GT, c.cell_algo) for c in details["Correct"]]
         self.assertEqual(2, len(correct_pairs))
-        self.assertIn((gt_data_1_frame[7], algo_data_1_frame[7]), correct_pairs)
-        self.assertIn((gt_data_1_frame[8], algo_data_1_frame[8]), correct_pairs)
+        self.assertEqualsCellsPairs((self.gt_data[7], self.algo_data[7]), correct_pairs[0], ignore_ids=True)
+        self.assertEqualsCellsPairs((self.gt_data[8], self.algo_data[8]), correct_pairs[1], ignore_ids=True)
 
         fp_pairs = [(c.cell_GT, c.cell_algo) for c in details["FalsePositive"]]
         self.assertEqual(5, len(fp_pairs))
-        self.assertIn((None, algo_data_1_frame[3]), fp_pairs)
-        self.assertIn((None, algo_data_1_frame[4]), fp_pairs)
-        self.assertIn((None, algo_data_1_frame[2]), fp_pairs)
-        self.assertIn((None, algo_data_1_frame[5]), fp_pairs)
-        self.assertIn((None, algo_data_1_frame[10]), fp_pairs)
+        self.assertEqualsCellsPairs((None, self.algo_data[3]), fp_pairs[1], ignore_ids=True)
+        self.assertEqualsCellsPairs((None, self.algo_data[4]), fp_pairs[2], ignore_ids=True)
+        self.assertEqualsCellsPairs((None, self.algo_data[2]), fp_pairs[0], ignore_ids=True)
+        self.assertEqualsCellsPairs((None, self.algo_data[5]), fp_pairs[3], ignore_ids=True)
+        self.assertEqualsCellsPairs((None, self.algo_data[10]), fp_pairs[4], ignore_ids=True)
 
         fn_pairs = [(c.cell_GT, c.cell_algo) for c in details["FalseNegative"]]
         self.assertEqual(2, len(fn_pairs))
-        self.assertIn((gt_data_1_frame[1], None), fn_pairs)
-        self.assertIn((gt_data_1_frame[9], None), fn_pairs)
+        self.assertEqualsCellsPairs((self.gt_data[1], None), fn_pairs[0], ignore_ids=True)
+        self.assertEqualsCellsPairs((self.gt_data[9], None), fn_pairs[1], ignore_ids=True)
 
     @skip("TODO")
     def test_single_evaluation_uses_ini_values(self):
--- tests/testbase.py
+++ tests/testbase.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 import imageio
+import numpy as np
 
 
 class TestBase(unittest.TestCase):
@@ -26,3 +27,30 @@ class TestBase(unittest.TestCase):
         right = position[0] + radius
         bottom = position[1] + radius
         image[top: bottom + 1, left: right + 1] = value
+
+    def save_in_platform_format(self, filename, cells):
+        with self.create_temp(filename) as file:
+            file.write("Frame_nr, Cell_nr, Cell_colour, Position_X, Position_Y\n")
+            for cell in cells:
+                file.write("{frame}, {cellid}, {color}, {posx}, {posy}\n".format(
+                    frame=cell.frame_number, cellid=cell.cell_id, posx=cell.position[0], posy=cell.position[1],
+                    color=cell.colour))
+
+    def save_in_mask_format(self, filename, cells, radius):
+        image = np.zeros((510, 510), dtype=np.uint8)
+        for cell in cells:
+            self.draw_cell(image, cell.position, radius, cell.colour + 1)
+        self.save_temp(filename, image)
+
+    def assertEqualsCells(self, a, b, ignore_ids=False):
+        if a is None or b is None:
+            self.assertEqual(a, b)
+        else:
+            if not ignore_ids:
+                self.assertEquals(a, b)
+            else:
+                self.assertEquals((a.position, a.colour), (b.position, b.colour))
+
+    def assertEqualsCellsPairs(self, a2, b2, ignore_ids=False):
+        self.assertEqualsCells(a2[0], b2[0], ignore_ids)
+        self.assertEqualsCells(a2[1], b2[1], ignore_ids)
